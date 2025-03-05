bd92466db909fa8135be24a05110af71dd14f72e
AhmedOsman101 <trollingosman101@outlook.com>
2025-03-05 15:49:02 +0200

    Updated Files 2025-03-05@03:49PM

--- auto-notify.plugin.zsh
+++ auto-notify.plugin.zsh
@@ -93,8 +93,8 @@ timeformat() {
     ;;
   1)
     echo 'text here 1'
-    # log-debug "${parts[0]}"
-    # log-debug "${parts[*]}"
+    log-debug "${parts[0]}"
+    log-debug "${parts[*]}"
     time_str="${parts[0]}"
     # log-debug "'${time_str}'"
     ;;
